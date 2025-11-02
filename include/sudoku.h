79378129421408ad2eb9fffa31bc11d7e24805aa
TuSha <pttu1704@gmail.com>
2025-11-02 22:18:24 +0700

    add difficulties
    add 3 difficulty options

--- include/sudoku.h
+++ include/sudoku.h
@@ -11,7 +11,9 @@ public:
     static const int SUBGRID_SIZE = 3;
 
     Sudoku();
+    // difficulty: 1 = Easy, 2 = Medium (default), 3 = Hard
     void generatePuzzle();
+    void generatePuzzle(int difficulty);
     bool isValid(int row, int col, int num) const;
     bool isCellEditable(int row, int col) const;
     bool setNumber(int row, int col, int num);
@@ -26,6 +28,7 @@ private:
     bool solveGrid();
     bool findEmptyCell(int &row, int &col) const;
     void removeCells();
+    void removeCells(int cellsToRemove);
     bool isValidInRow(int row, int num) const;
     bool isValidInCol(int col, int num) const;
     bool isValidInBox(int startRow, int startCol, int num) const;
